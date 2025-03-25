ced92ba1234ada4289e32364d016e3aabbe19a58
prashu0012 <118476213+prashu0012@users.noreply.github.com>
2025-03-26 00:59:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import Connection from './db/db.js';
 import router from './Router/route.js';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -12,6 +14,8 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', router);
 
+const port = process.env.PORT || 3000;
+
 Connection()
-    .then(() => app.listen(3000, () => { console.log(`listening..`) }))
+    .then(() => app.listen(port, () => { console.log(`listening..`) }))
     .catch((err) => console.log(err.message))
